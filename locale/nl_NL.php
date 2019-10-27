5dd2b05c06617139bf56e12daf2930f8fdaea9b0
François Kooman <fkooman@tuxed.net>
2019-10-27 13:07:18 +0100

    incorporate new design

---
+++ locale/nl_NL.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * eduVPN - End-user friendly VPN.
+ *
+ * Copyright: 2016-2019, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
+return [
+    'Continue...' => 'Verdergaan...',
+    'On the "Configurations" page, advanced users can manually download VPN configurations.' => 'Op de "Configuraties" pagina kunnen gevorderde gebruikers handmatig een VPN-configuratie downloaden.',
+    'On your "Account" page you can block access to the VPN in case you lose a device.' => 'Op je "Account" pagina kun je toegang tot het VPN blokkeren als je je device verliest.',
+    'To use Let\'s Connect!, download the app for your device below!' => 'Om Let\'s Connect! te gebruiken kun je de app voor jouw device hieronder downloaden!',
+    'Welcome to Let\'s Connect!' => 'Welkom bij Let\'s Connect!',
+];
