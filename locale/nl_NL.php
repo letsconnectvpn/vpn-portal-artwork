7ba4133d578c124f349368a529f6ff7a38fd7554
François Kooman <fkooman@tuxed.net>
2019-11-03 13:40:50 +0100

    translate "Download"

--- locale/nl_NL.php
+++ locale/nl_NL.php
@@ -9,6 +9,7 @@
 
 return [
     'Continue...' => 'Verdergaan...',
+    'Download' => 'Downloaden',
     'On the "Configurations" page, advanced users can manually download VPN configurations.' => 'Op de "Configuraties" pagina kunnen gevorderde gebruikers handmatig een VPN-configuratie downloaden.',
     'On your "Account" page you can block access to the VPN in case you lose a device.' => 'Op je "Account" pagina kun je toegang tot het VPN blokkeren als je je device verliest.',
     'To use Let\'s Connect!, download the app for your device below!' => 'Om Let\'s Connect! te gebruiken kun je de app voor jouw device hieronder downloaden!',
