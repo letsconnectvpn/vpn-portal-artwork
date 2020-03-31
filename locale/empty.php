417b2bb16bf0bf6854ff2e13e1d70d98a1382f5e
François Kooman <fkooman@tuxed.net>
2020-03-31 22:47:33 +0200

    add empty file

---
+++ locale/empty.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * eduVPN - End-user friendly VPN.
+ *
+ * Copyright: 2016-2019, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
+return [
+    'Continue...' => '',
+    'Download' => '',
+    'On the "Configurations" page, advanced users can manually download VPN configurations.' => '',
+    'On your "Account" page you can block access to the VPN in case you lose a device.' => '',
+    'To use Let\'s Connect!, download the app for your device below!' => '',
+    'Welcome to Let\'s Connect!' => '',
+];
