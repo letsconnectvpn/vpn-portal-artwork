fade87d0d9091c6eefcc69f24626423b5711cad4
François Kooman <fkooman@tuxed.net>
2020-06-29 22:15:07 +0200

    add pl_PL translation

---
+++ locale/pl_PL.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * eduVPN - End-user friendly VPN.
+ *
+ * Copyright: 2016-2020, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
+return [
+    'Android' => 'Android',
+    'Continue...' => 'Kontynuuj...',
+    'Linux' => 'Linux',
+    'On the "Configurations" page, advanced users can manually download VPN configurations.' => 'Zaawansowani użytkownicy mogą ręcznie pobrać konfigurację VPN na stronie "Konfiguracja".',
+    'On your "Account" page you can block access to the VPN in case you lose a device.' => 'W przypadku utraty urządzenia możesz zablokować dostęp do VPN na stronie "Konto".',
+    'To use Let\'s Connect!, download the app for your device below!' => 'Aby używać Let\'s Connect! pobierz aplikację dla Twojego urządzenia!',
+    'Welcome to Let\'s Connect!' => 'Witamy w Let\'s Connect!',
+    'Windows' => 'Windows',
+    'iOS' => 'iOS',
+    'macOS' => 'macOS',
+];
